51cdf76dee0773f698b996c1c2b619b59dea64fc
randallchacon <info@randallchacon.com>
2022-06-12 09:56:35 -0600

    Example of a mock abuse

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCostumer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+            var result = product.GetPrice(new Customer { IsGold = true });
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void GetPrice_GoldCostumer_Apply30PercentDiscount2() 
+            //Ex of Mock Abuse. Use mocks for remove external resources in the unit test
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(x => x.IsGold).Returns(true);
+
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
