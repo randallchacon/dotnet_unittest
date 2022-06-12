bc96e0fb1f82d6e337fcc407c4aa7449420ad88f
randallchacon <info@randallchacon.com>
2022-06-11 21:22:33 -0600

    Dependency injection by method parameters

---
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader : IFileReader //fake, mock or stub
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Mocking/VideoServicesTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServicesTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+            var result = service.ReadVideoTitle(new FakeFileReader());//Dependency Injection by parameter
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
